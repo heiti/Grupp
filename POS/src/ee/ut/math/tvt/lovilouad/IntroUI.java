78dec19e79105f2e553adad3ab11f932d7a4582d
Aigar Antson <aigarantson@gmail.com>
2013-10-08 14:15:32 +0300

    Application.properties failist saab kätte vajalikud andmed (pooleli...)

--- POS/src/ee/ut/math/tvt/lovilouad/IntroUI.java
+++ POS/src/ee/ut/math/tvt/lovilouad/IntroUI.java
@@ -2,16 +2,43 @@ package ee.ut.math.tvt.lovilouad;
 
 import javax.swing.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.awt.*;
+import java.util.Properties;
+
 public class IntroUI {
 
 	IntroUI() {
-		JFrame introFrame = new JFrame("The best POS in the World!");
+		Properties appProp = new Properties();
+		Properties verProp = new Properties();
+		
+		JFrame introFrame = new JFrame("POS Intro");
 		introFrame.setLayout(null);
 		introFrame.setSize(400,300);
 		introFrame.setLocation(400, 250);
 		introFrame.setResizable(false);
 		introFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		introFrame.setVisible(true);
+		
+		try {
+			appProp.load(new FileInputStream("application.properties"));
+			
+			JLabel teamName = new JLabel(appProp.getProperty("teamName"));
+			teamName.setFont(new Font("Arial", Font.PLAIN, 20));
+			teamName.setForeground(Color.BLACK);
+			teamName.setBounds(20, 20, 200, 20);
+			introFrame.add(teamName);
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 	//Team name
 	//Team leader
