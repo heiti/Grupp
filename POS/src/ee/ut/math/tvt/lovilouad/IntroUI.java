37b6e1b6d88707c6f04478dfa331bb5e92a23540
Aigar Antson <aigarantson@gmail.com>
2013-10-13 14:26:31 +0300

    lovilouad_logo.gif ja parandatud IntroUI

--- POS/src/ee/ut/math/tvt/lovilouad/IntroUI.java
+++ POS/src/ee/ut/math/tvt/lovilouad/IntroUI.java
@@ -20,7 +20,7 @@ public class IntroUI {
 		mainPanel.setBounds(0, 0, 500, 150);
 		mainPanel.setLayout(new GridLayout(5,1));
 		mainPanel.setBackground(new Color(248, 187, 14));
-		ImageIcon logoImage = new ImageIcon("images/lõvilõuad_logo.gif");
+		ImageIcon logoImage = new ImageIcon("images/lovilouad_logo.gif");
 		JLabel logoLabel = new JLabel(logoImage);
 		logoLabel.setBounds(0, 150, 500, 150);
 		introFrame.add(mainPanel);
@@ -29,8 +29,6 @@ public class IntroUI {
 		introFrame.setLocation(400, 250);
 		introFrame.setResizable(false);
 		introFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
 		
 		try {
 			appProp.load(new FileInputStream("application.properties"));
@@ -41,22 +39,20 @@ public class IntroUI {
 			e.printStackTrace();
 		}
 		
-		verProp.setProperty("build.number","Version " + verProp.getProperty("build.major.number") + "." + 
-				verProp.getProperty("build.minor.number") + "." + verProp.getProperty("build.revision.number"));
 		
-		JLabel teamName = new JLabel(appProp.getProperty("teamName"));
+		JLabel teamName = new JLabel("Team name: " + appProp.getProperty("teamName"));
 		teamName.setFont(new Font("Arial", Font.PLAIN, 15));
 		teamName.setForeground(Color.BLACK);
-		JLabel teamLeader = new JLabel(appProp.getProperty("teamLeader"));
+		JLabel teamLeader = new JLabel("Team leader: " + appProp.getProperty("teamLeader"));
 		teamLeader.setFont(new Font("Arial", Font.PLAIN, 15));
 		teamLeader.setForeground(Color.BLACK);
-		JLabel leaderEmail = new JLabel(appProp.getProperty("leaderEmail"));
+		JLabel leaderEmail = new JLabel("Leader email: " + appProp.getProperty("leaderEmail"));
 		leaderEmail.setFont(new Font("Arial", Font.PLAIN, 15));
 		leaderEmail.setForeground(Color.BLACK);
-		JLabel teamMembers = new JLabel(appProp.getProperty("teamMembers"));
+		JLabel teamMembers = new JLabel("Team members: " + appProp.getProperty("teamMembers"));
 		teamMembers.setFont(new Font("Arial", Font.PLAIN, 15));
 		teamMembers.setForeground(Color.BLACK);
-		JLabel buildVersion = new JLabel(verProp.getProperty("build.number"));
+		JLabel buildVersion = new JLabel("Version: " + verProp.getProperty("build.number"));
 		buildVersion.setFont(new Font("Arial", Font.PLAIN, 15));
 		buildVersion.setForeground(Color.BLACK);
 
@@ -68,12 +64,5 @@ public class IntroUI {
 		
 		introFrame.setVisible(true);
 	}
-	//Team name
-	//Team leader
-	//Team leader emali
-	//Team members
-	//Team logo (random image)
-	//Your software version number
-
 
 }
