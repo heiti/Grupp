12fa55b75035424f15405ce9ad8bb2e1ee96c17a
maarja_ <maarja.lepamets@ut.ee>
2013-10-13 17:02:39 +0300

    log4j.properties ja kogu loggimise asi

--- POS/src/ee/ut/math/tvt/lovilouad/Intro.java
+++ POS/src/ee/ut/math/tvt/lovilouad/Intro.java
@@ -1,10 +1,18 @@
 package ee.ut.math.tvt.lovilouad;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class Intro {
+	
+	private static final Logger log = Logger.getLogger(Intro.class);
 
 	public static void main(String[] args) {
+		PropertyConfigurator.configure("log4j.properties");
+		
 		IntroUI intro = new IntroUI();
-
+		
+		log.info("System executed!");
 	}
 
 }
