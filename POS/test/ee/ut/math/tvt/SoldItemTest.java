e8fe7ca0585818746517dd977aac0ff260ac0ce1
Heiti <heiti.tobi@ut.ee>
2013-11-22 18:45:23 +0200

    SoldItemTest, mõlemad testid tunduvad töötavat ilusti.

--- POS/test/ee/ut/math/tvt/SoldItemTest.java
+++ POS/test/ee/ut/math/tvt/SoldItemTest.java
@@ -1,5 +1,31 @@
 package ee.ut.math.tvt;
 
-public class SoldItemTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
+public class SoldItemTest {
+	private StockItem stock_item1;
+	private static double cost1= 15.00;
+	
+
+	@Before
+	public void setUp() {
+	stock_item1 = new StockItem(1L, "Lauaviin", "Kange alkohol", cost1);
+	}
+
+	@Test
+	public void testGetSum() {
+		SoldItem sold_item1 = new SoldItem(stock_item1, 3);
+		assertEquals(sold_item1.getSum(), (3*cost1), 0.0001);
+	}
+
+	@Test
+	public void testGetSumWithZeroQuantity() {
+		SoldItem sold_item1 = new SoldItem(stock_item1, 0);
+		assertEquals(sold_item1.getSum(), 0, 0.0001);	
+	}
 }
