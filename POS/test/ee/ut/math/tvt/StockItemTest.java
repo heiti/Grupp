a7b1275b981ad14931be1069fa58c35b0590b74f
Heiti <heiti.tobi@ut.ee>
2013-11-22 19:53:22 +0200

    StockItemtest, kõik toimib. Klooni testis kasutatud .getClass() meetodit, sest kloonides isendid ei võrdu üksteisega, aga klassid on samad.

--- POS/test/ee/ut/math/tvt/StockItemTest.java
+++ POS/test/ee/ut/math/tvt/StockItemTest.java
@@ -1,5 +1,32 @@
 package ee.ut.math.tvt;
 
-public class StockItemTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
+public class StockItemTest {
+	private StockItem item1;
+	private static Object items = 5;
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void testClone() {
+		StockItem item2 = new StockItem(1L, "Saku Originaal", "Ílu", 3, 5);
+		assertEquals(item2.clone().getClass(), item2.getClass());	
+		
+	}
+	
+	@Test
+	public void testGetColumn() {
+		item1 = new StockItem(1L, "Saku Originaal", "Ílu", 3, 5);
+		assertEquals(item1.getColumn(3), items);
+		
+	}
 }
