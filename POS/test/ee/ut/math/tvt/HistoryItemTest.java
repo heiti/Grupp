1b00e1c51f751967391b56755a23fdf8a076c41c
maarja_ <maarja.lepamets@ut.ee>
2013-11-23 17:31:13 +0200

    historyitemtest

--- POS/test/ee/ut/math/tvt/HistoryItemTest.java
+++ POS/test/ee/ut/math/tvt/HistoryItemTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ee.ut.math.tvt.salessystem.domain.data.HistoryItem;
@@ -13,23 +14,31 @@ import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class HistoryItemTest {
 	
-	private List<SoldItem> Solditems;
 	private static double  cost1 = 15.00;
 	private static double  cost2 = 12.00;
 	private static double totalSum = 39.00;
-	private static List<SoldItem> list1;
+	private List<SoldItem> Solditems;
+	private List<SoldItem> list1;
+	
+	StockItem stock_item1;
+	StockItem stock_item2;
+	SoldItem sold_item1;
+	SoldItem sold_item2;
 	
 	
 	
 	@Before
 	public void setUp() {
-	
+		stock_item1 = new StockItem(1001L, "Lauaviin", "Kange", cost1);
+		stock_item2 = new StockItem(1002L, "Torniviin", "Kange", cost2);
+		sold_item1 = new SoldItem(stock_item1, 1);
+		sold_item2 = new SoldItem(stock_item2, 2);
+		Solditems = new ArrayList<>();
+		list1 = new ArrayList<>();
 	}
 	
 	@Test
 	public void testAddSoldItem() {
-		StockItem stock_item1 = new StockItem(1001L, "Lauaviin", "Kange", cost1);
-		SoldItem sold_item1 = new SoldItem(stock_item1, 1);
 		HistoryItem sold = new HistoryItem(Solditems, 101L);
 		Solditems.add(sold_item1);
 		sold.setItems(Solditems);
@@ -38,14 +47,11 @@ public class HistoryItemTest {
 	@Test
 	public void testGetSumWithNoItems() {
 		HistoryItem sold = new HistoryItem();
-		assertEquals(sold.getTotalSum(), 0.0, 0.00);
-		
+		assertEquals(sold.getTotalSum(), 0.0, 0.00);	
 	}
 	
 	@Test
 	public void testGetSumWithOneItem() {
-		StockItem stock_item1 = new StockItem(1001L, "Lauaviin", "Kange", cost1);
-		SoldItem sold_item1 = new SoldItem(stock_item1, 1);
 		list1.add(0, sold_item1);
 		System.out.println(list1.get(0));
 		HistoryItem sold = new HistoryItem(list1, 1001L);
@@ -53,14 +59,9 @@ public class HistoryItemTest {
 	}
 	
 	@Test
-	public void testGetSumWithMultipleItems() {
-		StockItem stock_item1 = new StockItem(1001L, "Lauaviin", "Kange", cost1);
-		StockItem stock_item2 = new StockItem(1002L, "Torniviin", "Kange", cost2);
-		SoldItem sold_item1 = new SoldItem(stock_item1, 1);
-		SoldItem sold_item2 = new SoldItem(stock_item2, 2);
+	public void testGetSumWithMultipleItems() {		
 		Solditems.add(sold_item1);
-		Solditems.add(sold_item2);
-		
+		Solditems.add(sold_item2);		
 		HistoryItem sold2 = new HistoryItem(Solditems, 1002L);
 		assertEquals(sold2.getTotalSum(), totalSum, 0.001);
 		
